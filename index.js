9f7b3c761e2d230cd3f49617e27db72e9ee6806e
alrweashd <trkyabler@gmail.com>
2023-05-11 19:47:11 +0300

    Solved

---
+++ index.js
@@ -0,0 +1,61 @@
+//["Get Dry cleaning", "Review functions", "Review arrays"];
+let currentTodos = [];
+let doneTodos = [];
+//["Finish all coded tasks", "Sent important email"];
+const shows = (initailise) => {
+  if (initailise) {
+    currentTodos = ["Get Dry cleaning", "Review functions", "Review arrays"];
+    doneTodos = ["Finish all coded tasks", "Sent important email"];
+  }
+  console.log(
+    `Current todos: \n ${currentTodos} \nDone todos: \n ${doneTodos}\n`
+  );
+};
+shows(true);
+
+const completedTodo = (todo) => {
+  // find the string "todo" is it matching with currentTodos? yes? pop it and push it to the other array
+  // currentTodos.find((item,i)=>{
+  //     //console.log(item);
+  //     if (item == todo){
+  //         let x = currentTodos.splice(i,1);
+  //         doneTodos.push(x[0]);
+
+  //     } else console.log("not part of ur todos")
+
+  // })
+  if (currentTodos.includes(todo)) {
+    let x = currentTodos.indexOf(todo);
+    let y = currentTodos.splice(x, 1);
+    doneTodos.push(y[0]);
+  } else console.log("not part of ur todos");
+};
+completedTodo("Get Dry cleaning");
+shows();
+
+const validateTodo = (todo) => {
+  if (todo.length >= 2) return true;
+  else return false;
+};
+
+const addNewTodo = (todo) => {
+  if (validateTodo(todo)) currentTodos.push(todo);
+};
+addNewTodo("go to the gym");
+shows();
+
+const uncheckTodo = (undo) => {
+  if (currentTodos.includes(undo)) {
+    let x = doneTodos.indexOf(undo);
+    let y = doneTodos.splice(x, 1);
+    currentTodos.push(y[0]);
+  } else console.log("not part of ur doneTodos\n");
+};
+uncheckTodo("2");
+shows();
+
+const editTodo = (index, todoList, updatedTodo) => {
+  todoList[index] = updatedTodo;
+};
+editTodo(1, currentTodos, "Go home");
+shows();
